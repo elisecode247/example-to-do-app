9518ccfd5a52bdb2698de56478deec8ae2176a0f
elisecode247 <elisecode247@gmail.com>
2021-03-06 22:37:12 -0800

    fix quick config for heroku

--- config/index.js
+++ config/index.js
@@ -1,14 +1,21 @@
-const configPath = `${__dirname}/.env.${process.env.NODE_ENV}`;
-const config = require('dotenv').config({path: configPath });
+
 const { each } = require('lodash');
-
 let environmentVariables = {};
-
-if (!config.error) {
-    environmentVariables = config.parsed;
-} else {
-    console.error(`Environment error is: ${config.error}.`);
+if(process.env.NODE_ENV === 'production') {
     each(process.env, (value, key) => environmentVariables[key] = value);
+
+} else {
+    const configPath = `${__dirname}/.env.${process.env.NODE_ENV}`;
+    const config = require('dotenv').config({path: configPath });
+
+    if (!config.error) {
+        environmentVariables = config.parsed;
+    } else {
+        console.error(`Environment error is: ${config.error}.`);
+        each(process.env, (value, key) => environmentVariables[key] = value);
+    }
+
 }
+
 
 module.exports = environmentVariables;
